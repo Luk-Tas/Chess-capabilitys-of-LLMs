07fe5e4e8c34ad917fc0f6dc278fe1cc7c6ec2e8
Lukas Tasch <Lukastasch@gmx.de>
2023-03-27 13:59:53 +0200

    Implement a Way to change difficulty of the chess engine and write a script to let it play against it self on diffrent difficulties.

---
+++ Chess Engine/ChessEngine.py
@@ -0,0 +1,18 @@
+from stockfish import Stockfish
+
+class ChessEngine:
+
+    chess_level = ["beginner", "KIT_Professor"]
+
+    def __init__(self, engine_path):
+        self.stockfish = Stockfish(engine_path)
+
+    def change_difficulty(self, chess_level):
+        if chess_level == self.chess_level[0]:
+            self.stockfish.update_engine_parameters({"Skill Level": 1})
+        if chess_level == self.chess_level[1]:
+            self.stockfish.update_engine_parameters({"Skill Level": 13})
+
+    def get_best_move(self, board):
+        self.stockfish.set_fen_position(board)
+        return self.stockfish.get_best_move()
---
+++ Chess Engine/PlayChess.py
@@ -0,0 +1,28 @@
+import chess
+from ChessEngine import ChessEngine
+
+engine = ChessEngine\
+    (r"C:\Users\Lukas\Desktop\Ordner\Stockfish\stockfish_15.1_win_x64_avx2\stockfish-windows-2022-x86-64-avx2.exe")
+board = chess.Board()
+
+print(engine.stockfish.get_parameters())
+engine.change_difficulty("beginner")
+print(engine.stockfish.get_parameters())
+engine.change_difficulty("KIT_Professor")
+print(engine.stockfish.get_parameters())
+
+while not board.is_game_over():
+    engine.change_difficulty("beginner")
+    result = engine.get_best_move(board.fen())
+    print(result)
+    move = chess.Move.from_uci(result)
+    board.push(move)
+    engine.change_difficulty("beginner")
+    result = engine.get_best_move(board.fen())
+    print(result)
+    move = chess.Move.from_uci(result)
+    board.push(move)
+
+
+print(board.is_checkmate())
+
